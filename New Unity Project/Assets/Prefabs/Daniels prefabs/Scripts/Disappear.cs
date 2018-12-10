685e14dbf4a0e8a92cb22b7311b7b273e84221ef
HangedDragon96 <Menard.w@gmail.com>
2018-12-10 11:34:01 -0600

    level 4 and 5 commit
    testing level 4 and 5 commit

---
+++ New Unity Project/Assets/Prefabs/Daniels prefabs/Scripts/Disappear.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disappear: MonoBehaviour
+{
+    public GameObject GameObjectToHide;
+    private bool active = false;
+    void OnEnable()
+    {
+        StartCoroutine(ToggleVisibilityCo(GameObjectToHide));
+    }
+    
+    IEnumerator ToggleVisibilityCo(GameObject someObj)
+    {
+        if (someObj == null) yield break;
+
+        while (true)
+        {
+            if (active == false)
+            {
+                someObj.SetActive(active);
+                active = true;
+                yield return new WaitForSeconds(8.0f);
+            } else
+            {
+                someObj.SetActive(active);
+                active = false;
+                yield return new WaitForSeconds(8.0f);
+            }
+            
+        }
+
+    }
+}
