1c019eb0d48df2d24997407ae37b16b6dd8acadb
Hidingrighthere <benbneben@gmail.com>
2018-12-10 01:22:26 -0600

    test

---
+++ New Unity Project/Assets/Ben's Scripts/CreateLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLevel : MonoBehaviour
+{
+    Material dissolve;
+    private float currDissolve = 1.0f;
+    public Vector3 target;
+
+    public float speed;
+    public int iMove; //0 to move to the left of right, 1 to move up or down 
+
+    public float t;
+    public float bpm;
+
+    // Use this for initialization
+	private void Start ()
+    {
+        dissolve = GetComponent<Renderer>().material;
+        Debug.Log(target);
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        float step = speed * Time.deltaTime;
+        currDissolve -= Time.deltaTime;
+        dissolve.SetFloat("_DissolveAmount", currDissolve);
+
+        t += (Time.deltaTime * 60f/bpm)/2;
+        transform.position = Vector3.Lerp(transform.position, target, t);
+
+
+    }
+}
---
+++ New Unity Project/Assets/Ben's Scripts/FlameWings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameWings : MonoBehaviour
+{
+    public GameObject FireWings; //Fire wing prefab to be made
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	        	
+	}
+
+
+    //Spawns wings and add it to the character
+    private void SpawnWings()
+    {
+
+    }
+
+
+    //Removes the wings when landing
+    private void DeSpawnWings()
+    {
+
+    }
+}
